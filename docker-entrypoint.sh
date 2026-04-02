631ac55dcc00c0934b0c9d763cc7f722a40882a5
Akram Youssoufi <akramyoussoufi1@gmail.com>
2026-04-02 15:05:29 +0100

    fixing deployement

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 set -e
 
-# ensure env exists
-if [ ! -f .env ]; then
-  cp .env.example .env
-fi
-
-# ensure key exists
-php artisan key:generate --force
-
 echo "Waiting for database..."
-
 until php artisan migrate:status > /dev/null 2>&1; do
   sleep 3
 done
-
 echo "Database ready"
 
 php artisan migrate --force
-
 php artisan config:cache
 php artisan route:cache
 
