68b54826a3bf8bbf6307a785b3582ead17ed82ed
Akram Youssoufi <akramyoussoufi1@gmail.com>
2026-04-02 15:49:11 +0100

    fixing deployement

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    'allowed_origins' => [
+        'http://127.0.0.1:5500',
+        'http://localhost:5500',
+        'https://hinweisgeberportals-projekt-ui.onrender.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
